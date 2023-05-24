3642c44df0fd12d17504566f228c0d86f770d489
Luis <luisperi18@hotmail.com>
2023-05-24 20:52:42 +0200

    Add card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Home } from "./component/home/home";
-
+import { Card } from "./component/character.card/card";
 function App() {
   return (
     <>
       <Home></Home>
+      <Card></Card>
     </>
   );
 }
